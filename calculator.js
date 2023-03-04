a89c7b4aaaa8f0b61731a3399b9dcba9e18aba04
perugi <dominik.perusko@gmail.com>
2023-03-04 22:05:19 +0100

    Implement functionality for digit and clear/backspace buttons

--- calculator.js
+++ calculator.js
@@ -29,3 +29,37 @@ function operate(a, b, operator) {
     }
 }
 
+let displayValue = '0';
+
+digitBtns = document.querySelectorAll('.digit');
+digitBtns.forEach(btn => btn.addEventListener('click', (e) => {
+    if (displayValue === '0') {
+        displayValue = e.target.textContent;
+    } else {
+        displayValue += e.target.textContent;
+    }
+
+    refreshDisplay();
+}));
+
+clearBtn = document.querySelector('#clear');
+clearBtn.addEventListener('click', () => {
+    displayValue = '0';
+
+    refreshDisplay();
+})
+bckspBtn = document.querySelector('#backspace');
+bckspBtn.addEventListener('click', () => {
+    if (displayValue.length > 1) {
+        displayValue = displayValue.slice(0, -1);
+    } else {
+        displayValue = '0';
+    }
+
+    refreshDisplay();
+})
+
+function refreshDisplay() {
+    display = document.querySelector('#display');
+    display.textContent = displayValue;
+}
